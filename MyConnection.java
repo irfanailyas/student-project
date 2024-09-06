341665d0d55d3b31bcecf6b060e666e3ef7d04c0
irfanailyas <158738857+irfanailyas@users.noreply.github.com>
2024-09-06 23:56:20 +0530

    Add files via upload

---
+++ MyConnection.java
@@ -0,0 +1,24 @@
+
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MyConnection {
+    private static final String username= "root";
+    private static final String password= "1212";
+    private static final String dataConn = "jdbc:mysql://localhost:3306/student_management";
+    private static Connection con = null;
+    
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con=DriverManager.getConnection(dataConn, username, password);
+            
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return con;
+    }
+    
+}
---
+++ Score.java
@@ -0,0 +1,167 @@
+package student;
+
+import db.MyConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import static student.Home.jTextField24;
+
+public class Score {
+
+    Connection con = MyConnection.getConnection();
+    PreparedStatement ps;
+
+    public int getMax() {
+        int id = 0;
+        Statement st;
+        try {
+            st = con.createStatement();
+            ResultSet rs = st.executeQuery("select max(id) from score");
+            while (rs.next()) {
+                id = rs.getInt(1);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Score.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return id + 1;
+    }
+
+    public boolean getDetails(int sid, int semesterNo) {
+        try {
+            ps = con.prepareStatement("select * from course where student_id=? and semester = ?");
+            ps.setInt(1, sid);
+            ps.setInt(2, semesterNo);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                Home.jTextField24.setText(String.valueOf(rs.getInt(2)));
+                Home.jTextField25.setText(String.valueOf(rs.getInt(3)));
+                Home.jTextCourse1.setText(rs.getString(4));
+                Home.jTextCourse2.setText(rs.getString(5));
+                Home.jTextCourse3.setText(rs.getString(6));
+
+                return true;
+            } else {
+                JOptionPane.showMessageDialog(null, "Student id or  semester number doesn't exist");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Score.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+
+    //get all the score values from database score table
+    public void getScoreValue(JTable table, String searchValue) {
+        String sql = "select * from score where concat(id,student_id,semester) like ? order by id desc";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, "%" + searchValue + "%");
+            ResultSet rs = ps.executeQuery();
+            DefaultTableModel model = (DefaultTableModel) table.getModel();
+            Object[] row;
+            while (rs.next()) {
+                row = new Object[10];
+                row[0] = rs.getInt(1);
+                row[1] = rs.getInt(2);
+                row[2] = rs.getInt(3);
+                row[3] = rs.getString(4);
+                row[4] = rs.getDouble(5);
+                row[5] = rs.getString(6);
+                row[6] = rs.getDouble(7);
+                row[7] = rs.getString(8);
+                row[8] = rs.getDouble(9);
+                row[9] = rs.getDouble(10);
+                model.addRow(row);
+
+            }
+        } catch (SQLException ex) {
+
+            Logger.getLogger(Score.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    //check whether the student id or semester number already exists
+    public boolean isSidSemesterNoExist(int sid, int semesterNo) {
+        try {
+            ps = con.prepareStatement("select * from score where student_id=? and semester=?");
+            ps.setInt(1, sid);
+            ps.setInt(2, semesterNo);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return true;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Score.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+
+//insert data into score table
+    public void insert(int id, int sid, int semester, String course1, String course2,
+            String course3, double score1, double score2, double score3, double average) {
+        String sql = "insert into score values(?,?,?,?,?,?,?,?,?,?)";
+        try {
+
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            ps.setInt(2, sid);
+            ps.setInt(3, semester);
+            ps.setString(4, course1);
+            ps.setDouble(5, score1);
+            ps.setString(6, course2);
+            ps.setDouble(7, score2);
+            ps.setString(8, course3);
+            ps.setDouble(9, score3);
+            ps.setDouble(10, average);
+            if (ps.executeUpdate() > 0) {
+                JOptionPane.showMessageDialog(null, "Scores added successfully");
+
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Score.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    //check  student id already exists
+    public boolean isIdExist(int id) {
+        try {
+            ps = con.prepareStatement("select * from score where id=?");
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return true;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Score.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+
+    //update score value
+    public void update(int id, double score1, double score2, double score3, double average) {
+        String sql = "update score set score1=?,score2=?,score3=? average=? where id=? ";
+        try {
+
+            ps = con.prepareStatement(sql);
+            ps.setDouble(1, score1);
+            ps.setDouble(2, score2);
+            ps.setDouble(3, score3);
+            ps.setDouble(4, average);
+            ps.setInt(5, id);
+
+            if (ps.executeUpdate() > 0) {
+                JOptionPane.showMessageDialog(null, "Score updated successfully");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Score.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+}
